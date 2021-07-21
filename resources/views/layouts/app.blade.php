9caef7aa3eedddaa9ad72e9c70c69fe09973d173
erripollo <pollone.enrico@gmail.com>
2021-07-22 01:57:45 +0200

    modificate navbar in layouts.app and layouts.admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,41 +22,83 @@
 <body class="bg-dark">
     <div id="app" class="bg-dark">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Boolpress</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item {{Route::currentRouteName() === 'home' ? 'active' : '' }}">
-                  <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item {{Route::currentRouteName() === 'posts.index' ? 'active' : '' }}">
-                  <a class="nav-link" href="{{route('posts.index')}}">Posts</a>
-                </li>
-                <li class="nav-item {{Route::currentRouteName() === 'about' ? 'active' : '' }}">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item {{Route::currentRouteName() === 'contacts' ? 'active' : '' }}">
-                    <a class="nav-link" href="#">Contacts</a>
-                </li>
-              </ul>
-              <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-              </form>
+            <div class="container">
+
+                <a class="navbar-brand mr-5" href="#">Boolpress</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item {{Route::currentRouteName() === 'home' ? 'active' : '' }}">
+                      <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item {{Route::currentRouteName() === 'posts.index' ? 'active' : '' }}">
+                      <a class="nav-link" href="{{route('posts.index')}}">Posts</a>
+                    </li>
+                    <li class="nav-item {{Route::currentRouteName() === 'about' ? 'active' : '' }}">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item {{Route::currentRouteName() === 'contacts' ? 'active' : '' }}">
+                        <a class="nav-link" href="#">Contacts</a>
+                    </li>
+                  </ul>
+                  <!-- Right Side Of Navbar -->
+                  <ul class="navbar-nav ml-auto mr-5">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+    
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            
+                                <a class="dropdown-item" href="{{ route('admin.dashboard') }}">
+                                    Admin
+                                </a>
+                        
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+                </div>
             </div>
           </nav>
 
           <div class="jumbotron">
-              <h1 class="display-3">Jumbo heading</h1>
-              <p class="lead">Jumbo helper text</p>
-              <hr class="my-2">
-              <p>More info</p>
-              <p class="lead">
-                  <a class="btn btn-outline-success btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
-              </p>
+              <div class="container">
+                  <h1 class="display-3">Jumbo heading</h1>
+                  <p class="lead">Jumbo helper text</p>
+                  <hr class="my-2">
+                  <p>More info</p>
+                  <p class="lead">
+                      <a class="btn btn-outline-success btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                  </p>
+              </div>
           </div>
 
         <main class="py-4">
