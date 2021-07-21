68917b83295d066bf9581ce881e2f407cf29fd09
erripollo <pollone.enrico@gmail.com>
2021-07-21 09:05:45 +0200

    create cards structure in guest.posts.index page

--- resources/views/guest/posts/index.blade.php
+++ resources/views/guest/posts/index.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
        <div class="row">
             @foreach ($posts as $post)
-                <div class="col-md-4">
+                <div class="col-md-4 mb-4">
                     <div class="card text-left">
                         <img class="card-img-top" src="{{$post->image}}" alt="{{$post->title}}">
                         <div class="card-body">
