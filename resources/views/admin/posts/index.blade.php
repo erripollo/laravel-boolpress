6856c8cdc4a77829558609c9841b3bb19d057cdc
erripollo <pollone.enrico@gmail.com>
2021-07-21 14:47:34 +0200

    Display table with created posts in admin.posts.index view

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,6 +2,34 @@
 
 @section('content')
 
-    <h1>Posts admin page</h1>
+    <div class="container">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th>Summary</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <td scope="row">{{$post->id}}</td>
+                        <td><img width="100" src="{{$post->image}}" alt="{{$post->title}}"></td>
+                        <td>{{$post->title}}</td>
+                        <td>{{$post->summary}}</td>
+                        <td>
+                            <a href="#">View</a>|
+                            <a href="#">Edit</a>|
+                            <a href="#">Delete</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    
     
 @endsection
