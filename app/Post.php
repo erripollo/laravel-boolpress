a9ed473d4b64f654bb5083fd97188deb7012da03
erripollo <pollone.enrico@gmail.com>
2021-07-26 18:22:14 +0200

    Create relation between tables of posts and categories

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'image', 'summary', 'body'];
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    
 }
