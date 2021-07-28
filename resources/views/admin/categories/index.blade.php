1656057fc706586f3ac68e4302d1de4edb7c4671
erripollo <pollone.enrico@gmail.com>
2021-07-29 01:16:36 +0200

    create table and display categories in admin.categories.index view

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
 
-    <h1>Category page</h1>
-    
+    <div class="container">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Slug</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $category)
+                    <tr>
+                        <td scope="row">{{ $category->id }}</td>
+                        <td>{{ $category->name }}</td>
+                        <td>{{ $category->slug }}</td>
+                        <td>Show | Edit | Delete</td>
+                    </tr>    
+                @endforeach
+            </tbody>
+        </table>
+    </div>    
+
 @endsection
