8f3eeb2b09d9e46a25a114f82d64307788ac4722
erripollo <pollone.enrico@gmail.com>
2021-07-29 16:07:20 +0200

    Add erros allert on admin.categories.create form

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -5,14 +5,28 @@
     <div class="container">
         <h1 class="mb-4">Create a new category</h1>
 
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+            
+        @endif
+
         <form action="{{ route('admin.categories.store') }}" method="post">
             @csrf
     
             <div class="form-group">
               <label for="name">Name</label>
-              <input type="text" class="form-control" name="name" id="name" aria-describedby="nameHelper" placeholder="Name category">
+              <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" aria-describedby="nameHelper" placeholder="Name category" value="{{ old('name') }}" required>
               <small id="nameHelper" class="form-text text-muted">Type a name for this category</small>
             </div>
+            @error('name')
+                <div class="alert alert-danger">{{$message}}</div>
+            @enderror
 
             
             <button type="submit" class="btn btn-primary btn-block">Submit</button>
