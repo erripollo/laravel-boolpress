3cade413bc47b975b3f0c5f606f4dca075a56a11
erripollo <pollone.enrico@gmail.com>
2021-07-30 19:19:13 +0200

    display posts with v-for in blog view

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,11 +3,19 @@
 @section('jumbotron')
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
-            <h1 class="display-3">Blog</h1>
+            <h1 class="display-3">Vue Blog</h1>
         </div>
     </div>
 @endsection
 
 @section('content')
-    <h2>Vue Posts</h2>    
+    <div class="container d-flex flex-wrap justify-content-center">
+        <div class="card text-left m-2" v-for="post in posts" style="width: 350px;">
+            <img class="card-img-top" :src=" 'storage/' + post.image " :alt="post.title">
+            <div class="card-body px-1">
+                <h4 class="card-title">@{{post.title}}</h4>
+                <p class="card-text">@{{post.summary}}</p>
+            </div>
+        </div>
+    </div> 
 @endsection
